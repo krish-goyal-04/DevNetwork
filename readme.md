e000e17a3a0abdea2e1bcec450306702210dfe96
Krish Goyal <krishofficial4aug@gmail.com>
2026-02-21 14:30:46 +0530

    Built basic APIS

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@ npm i express
 running epress/node files node src/app.js
 
 using mongoose to talk to mongodb database
+
+built basic apis, but data sanitation is needed.
--- src/app.js
+++ src/app.js
@@ -2,23 +2,72 @@ const express = require("express");
 
 const { connectDB } = require("./config/database");
 const { User } = require("./models/user");
+
 const app = express();
 
-app.post("/signup", async (req, res) => {
-  const dummyObj = {
-    firstName: "harishhh",
-    lastName: "Goyal",
-    gender: "Male",
-    age: 22,
-    emailId: "harish@123",
-  };
+app.use(express.json());
 
+//API to create a new user
+app.post("/signup", async (req, res) => {
+  //sending dynamic data for signup
   try {
-    const newUser = new User(dummyObj);
+    const doesUserExists = await User.exists({ emailId: req.body.emailId });
+    if (doesUserExists != null) {
+      res.send("User already exists!");
+      return;
+    }
+    const newUser = new User(req.body);
     await newUser.save();
     res.send("User Successfully added!");
   } catch (err) {
     res.status(400).send(err.message);
+  }
+});
+
+//API to get a unique user details usin email id
+app.get("/user", async (req, res) => {
+  try {
+    const userEmailId = req.body.emailId;
+    const userDetails = await User.findOne({ emailId: userEmailId });
+    console.log(userDetails);
+    userDetails
+      ? res.send(userDetails)
+      : res.send("User with this email doesnot exist");
+  } catch (err) {
+    res.status(400).send("User Not Found!");
+  }
+});
+
+//API to get all users,, Feed API
+app.get("/feed", async (req, res) => {
+  try {
+    const allUsers = await User.find();
+    allUsers ? res.send(allUsers) : res.status(404).send("No user found");
+  } catch (err) {
+    res.send("Error fetching all users!");
+  }
+});
+
+//API to find user by id and update(PATCH)
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate(userId, data);
+    res.send("Data updated successfully!");
+  } catch (err) {
+    res.status(400).send("Failed to update data!");
+  }
+});
+
+//API to delete a user by user id
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted Successfully!");
+  } catch (err) {
+    res.status(400).send("Encontered an error in deleting the user!");
   }
 });
 
