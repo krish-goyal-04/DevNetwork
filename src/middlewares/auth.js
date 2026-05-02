1de05fd9b97968c987f31d0fa38e77c79f934029
Krish Goyal <krishofficial4aug@gmail.com>
2026-05-02 16:49:56 +0530

    Modified userAuth middleware with proper status code and json messages in response

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -12,23 +12,23 @@ const userAuth = async (req, res, next) => {
     //The token here is a jwt token of format header.secret message.digital signature
     const cookies = req.cookies;
     const { token } = cookies;
-    if (!token) throw new Error("Invalid Token!!!");
+    if (!token) return res.status(401).json({ message: "No token provided!!" });
 
     //Using the token which contains user id to check wetheruser is logged in or not
     //first we will check wether the token exists in the cookies
     //if user does not exist then we will simply reject the user's request to access any page and then send error as a reponse
     //then extract the user id from it
     const decodedObj = jwt.verify(token, process.env.JWT_Private_Key);
-    if (!decodedObj) throw new Error("User not found in cookie!");
+    if (!decodedObj) return res.status(401).json({ message: "Invalid token" });
 
     //Extracting the user id from token
     const { _id } = decodedObj;
-    if (!_id) throw new Error("User not found!!");
+    if (!_id) return res.status(401).json({ message: "User not found!!" });
     //here we get confirmed that user exists, now we need to verify wether the requesting user and the logged in user are same
 
     //Once we have received the id of user, we will check the database if the user exists
     const user = await User.findById(_id);
-    if (!user) throw new Error("User not found!!");
+    if (!user) return res.status(401).json({ message: "User not found!!" });
     //Till here if no errors are found, this implies that the user is verified and his/her request can be processed furthur
     //to the requested HTTP method
     //we will assign the user to the HTTP method byy attaching it with th request
@@ -36,7 +36,9 @@ const userAuth = async (req, res, next) => {
     req.user = user;
     next();
   } catch (err) {
-    res.send("ERROR : " + err.message);
+    return res.status(401).json({
+      message: err.message || "Authentication failed",
+    });
   }
 };
 
