0f73a1e557d3e94fe3e95e6011cebbf8d9caebd5
Krish Goyal <krishofficial4aug@gmail.com>
2026-06-17 01:46:47 +0530

    cookies updated

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -103,6 +103,8 @@ authRouter.post("/logout", userAuth, async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
+      secure: true,
+      sameSite: "none",
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // Cookie expires in 7 days
     });
     return res.status(200).json({ message: "Logged out successfully !!" });
